e7f1e434a837e2c0e8c4cc68d283d9dbb9f3fe68
Tomasz <mandalorian99@gmail.com>
2022-02-09 11:07:50 +0000

    header finished

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,6 +3,9 @@ import "./Header.scss";
 import insta from "../../assets/images/instagram.svg";
 import { IoPaperPlaneOutline, IoHeartOutline } from "react-icons/io5";
 import { CgAddR } from "react-icons/cg";
+import { ImCompass2 } from "react-icons/im";
+import { GrHomeRounded } from "react-icons/gr";
+import av from "../../assets/images/me1.jpg";
 
 const Header = () => {
   return (
@@ -12,9 +15,13 @@ const Header = () => {
 
       {/* right */}
       <div className="header__icons">
-        <CgAddR className="header__icon" />
-        <IoHeartOutline className="header__icon" />
+        <GrHomeRounded className="header__icon" />
         <IoPaperPlaneOutline className="header__icon" />
+        <CgAddR className="header__icon" />
+        <ImCompass2 className="header__icon" />
+        <IoHeartOutline className="header__icon" />
+
+        <img src={av} alt="Profile Avatar" className="header__avatar" />
       </div>
     </div>
   );
