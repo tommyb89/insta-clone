c87b9b59bb7c5fb9d8f9d70388e444fce90dadd5
Tomasz <mandalorian99@gmail.com>
2022-02-09 20:50:51 +0000

    added mock data file

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import "./App.scss";
 import Header from "./components/Header/Header";
 import Profile from "./components/Profile/Profile";
+import { userData } from "./data/data.js";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Profile />
+      <Header user={userData} />
+      <Profile user={userData} />
     </div>
   );
 }
